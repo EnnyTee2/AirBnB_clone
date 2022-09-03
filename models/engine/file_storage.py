56d3b61c521709e98e8ee682e7fdf9a8beb1e9d2
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-09-03 20:39:40 +0100

    Create file_storage.py

---
+++ models/engine/file_storage.py
@@ -0,0 +1,39 @@
+import json
+
+class FileStorage:
+    ''' This class serializes instances to a JSON file
+        and deserializes JSON file to instances, It thus
+        represents an abstracted storage engine.
+
+        Attributes:
+        __file_path (str): The path to the name of the file to save objects to.
+        __objects (dict): A dictionary of instantiated objects.
+    '''
+
+
+    __filepath = 'airbnb_clone.json'
+    __objects = {}
+
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        """Set in __objects obj with key <obj_class_name>.id."""
+        self.__objects['{}.{}'.format(type(obj).__name__, obj.id)] = obj
+
+    def save(self):
+        """Serialize __objects to the JSON file __file_path."""
+        dicobject = {}
+        for key in self.__objects:
+            dicobject[key] = self.__objects[key].
+        with open(self.__filepath, 'w', encoding ='utf8') as json_file:
+            json.dump(dicobject, json_file)
+
+    def reload(self):
+        """Deserialize the JSON file __file_path to __objects, if it exists."""
+        try:
+            with open(self.__filepath, 'r', encoding ='utf8') as json_file:
+                json.load(json_file)
+        except FileNotFoundError:
+            pass
