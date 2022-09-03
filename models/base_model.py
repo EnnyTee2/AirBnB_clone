632324a5db18165fe969a13381788c2f6fb13a47
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-09-03 20:31:52 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,38 +1,30 @@
-import uuid
+from uuid import uuid4
 from datetime import datetime
 
 class BaseModel:
     """
+        Defines the BaseModel class for all objects
     """
 
+
     def __init__(self, *args, **kwargs):
-        if kwargs == {}:
-            my_id = uuid.uuid4()
-            self.id = str(my_id)
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
+        """Base model initialization"""
+        self.id = str(uuid4())
+        self.created_at = self.updated_at = datetime.now()
+        if kwargs:
             for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                elif key == 'created_at':
-                    setattr(self, key, self.formatter(value))
-                elif key == 'updated_at':
-                    setattr(self, key, self.formatter(value))
-                else:
+                if key != '__class__':
                     setattr(self, key, value)
+                if key == 'created_at' or key == 'updated_at':
+                    time_format = '%Y-%m-%dT%H:%M:%S.%f'
+                    setattr(self, key, datetime.strptime(value, time_format))                    
 
     def save(self):
+        """Updates updated_at with the current datetime object"""
         self.updated_at = datetime.now()
 
-    def formatter(self, time):
-        time_f = time[2:]
-        time_f = time_f.replace('T', ' ')
-        time_f = time_f.replace('-', '/')
-        time_format = '%y/%m/%d %H:%M:%S.%f'
-        return datetime.strptime(time_f, time_format)
-
     def to_dict(self):
+        """Converts all class attributes to a dictionary (json format)"""
         dicto = {'__class__': type(self).__name__ ,
                  'created_at': datetime.isoformat(self.created_at),
                  'updated_at': datetime.isoformat(self.updated_at)
@@ -42,4 +34,7 @@ class BaseModel:
         return dict_rep
 
     def __str__(self):
-        print(f"[{type(self).__name__}] ({self.id}) {self.__dict__}")
+        """ overriding the default str method to define the 
+            string representation of the BaseModel class
+        """
+        return (f"[{type(self).__name__}] ({self.id}) {self.__dict__}")
